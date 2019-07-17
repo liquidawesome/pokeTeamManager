64ee040edf5396c4acff9827ad0fbe2de9a30716
Sean McCartan <sean2223@gmail.com>
2019-07-17 13:35:39 -0400

    Install parcel for bundling, begin controller, setup npm commands

---
+++ js/index.js
@@ -0,0 +1,14 @@
+import Find from './models/Find';
+
+const state = {};
+
+/* POKEMON CONTROLLER */
+
+
+/* SEARCH CONTROLLER */
+
+
+/* DETAIL CONTROLLER */
+
+
+/* EVENT HANDLERS */
---
+++ js/models/Find.js
@@ -0,0 +1,7 @@
+export default class Find {
+	constructor(query) {
+		this.query = query;
+	}
+
+
+}
